490174a5d4b0d63c01756724d6c80b489ac4ea85
NicoDev92 <alenolmedo92@gmail.com>
2025-01-14 12:05:44 -0300

    Agregando PaymentRepository.java para resolver conflictos

---
+++ backend/src/main/java/com/sistema/gestion/Repositories/Admin/Finance/PaymentRepository.java
@@ -0,0 +1,26 @@
+package com.sistema.gestion.Repositories.Admin.Finance;
+
+import java.time.LocalDate;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistema.gestion.Models.Admin.Finance.Payment;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface PaymentRepository extends ReactiveMongoRepository<Payment, String> {
+
+        Flux<Payment> findAllByStudentId(String studentId);
+
+        Flux<Payment> findAllByHasDebt(Boolean hasDebt);
+
+        Mono<Long> countAllByHasDebt(Boolean hasDebt);
+
+        @Query("{ 'hasDebt': true, 'paymentDueDate': { $gte: ?0, $lt: ?1 } }")
+        Flux<Payment> findAllWithDebtInMonth(LocalDate startOfMonth, LocalDate endOfMonth);
+
+}
