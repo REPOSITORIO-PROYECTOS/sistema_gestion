ace67681b6adb2bbf85112aaeaed0e580475a577
Juanmorales1810 <juan1810.jm@gmail.com>
2025-01-21 12:14:26 -0300

    Feat: Se agregó un componente Panel en la página de cursos

--- frontend/src/app/cursos/page.tsx
+++ frontend/src/app/cursos/page.tsx
@@ -1,10 +1,12 @@
 import AgregarCurso from '@/components/agregar-curso'
+import Panel from '@/components/interfaces/panel'
 import TableFilter from '@/components/interfaces/table-filter'
 import React from 'react'
 
 export default function Page() {
     return (
         <>
+            <Panel />
             <AgregarCurso />
             <TableFilter />
         </>
