aafcac7c9e10f9015d8751c0832ac7a304be4fe9
Juanmorales1810 <juan1810.jm@gmail.com>
2025-01-21 11:49:02 -0300

    Feat: Se agregó una nueva página con componentes para agregar cursos y filtrar tablas

---
+++ frontend/src/app/cursos/page.tsx
@@ -0,0 +1,12 @@
+import AgregarCurso from '@/components/agregar-curso'
+import TableFilter from '@/components/interfaces/table-filter'
+import React from 'react'
+
+export default function Page() {
+    return (
+        <>
+            <AgregarCurso />
+            <TableFilter />
+        </>
+    )
+}
