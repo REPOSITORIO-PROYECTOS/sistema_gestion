d361d63633cd0471b7dd80c51027fce0a69a3950
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-01-26 23:09:34 -0300

    Feat: Se reemplazó el componente TableFilter por TablePerson en la página de inicio

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import Estadisticas from "@/components/interfaces/estadisticas";
 import Panel from "@/components/interfaces/panel";
-import TableFilter from "@/components/interfaces/table-filter";
+import TablePerson from "@/components/interfaces/table-person";
 
 export default function Home() {
   return (
     <>
       <Panel />
       <Estadisticas />
-      <TableFilter />
+      <TablePerson />
     </>
   );
 }
