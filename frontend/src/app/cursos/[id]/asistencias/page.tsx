8a1ad8a24e3ce440abfc5d788641e95a1b75df7f
Juanmorales1810 <juan1810.jm@gmail.com>
2025-02-07 12:07:35 -0300

    feat: agregar ID de curso a la página de asistencia y actualizar el título

--- frontend/src/app/cursos/[id]/asistencias/page.tsx
+++ frontend/src/app/cursos/[id]/asistencias/page.tsx
@@ -1,10 +1,15 @@
 import { TablaAsistencia } from "@/components/interfaces/tabla-asistencia"
 
-export default function AsistenciaPage() {
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ id: string }>
+}) {
+    const id = (await params).id
     return (
         <div className="container mx-auto py-10">
-            <h1 className="text-2xl font-bold mb-5">Control de asistencia diarias</h1>
-            <TablaAsistencia />
+            <h1 className="text-2xl font-bold mb-5">Control de asistencia diarias {id}</h1>
+            <TablaAsistencia id={id} />
         </div>
     )
 }
