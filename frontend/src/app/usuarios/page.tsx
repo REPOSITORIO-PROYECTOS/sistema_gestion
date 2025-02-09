a7e2ef7847456705cc75f393951ae10d2d5b9857
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-02-09 12:38:34 -0300

    feat: agregar encabezado al panel de usuarios

--- frontend/src/app/usuarios/page.tsx
+++ frontend/src/app/usuarios/page.tsx
@@ -1,10 +1,13 @@
-import Panel from "@/components/interfaces/panel";
 import TableUsers from "@/components/interfaces/table-users";
 
 export default function Page() {
     return (
         <>
-            <Panel />
+            <div className="container flex flex-col gap-12 mx-auto p-16">
+                <h2 className="text-[#1e1e1e] text-2xl font-semibold">
+                    PANEL DE USUARIOS
+                </h2>
+            </div>
             <TableUsers />
         </>
     );
