682cc33bc17681957f4c617dd15f79d1ccb3e8d1
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-21 12:18:24 -0300

    feat: agregar página de cursos con tabla de cursos

---
+++ frontend/src/app/admin/cursos/page.tsx
@@ -0,0 +1,14 @@
+import TableCursos from "@/components/interfaces/table-cursos";
+
+export default function Page() {
+    return (
+        <section className="container flex flex-col gap-12 mx-auto p-16">
+            <div>
+                <h2 className="text-[#1e1e1e] text-2xl font-semibold">
+                    PANEL DE CURSOS
+                </h2>
+            </div>
+            <TableCursos />
+        </section>
+    );
+}
