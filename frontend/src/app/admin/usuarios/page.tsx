7094e1509b7022a358a2bcb49bc817bc08a9ab60
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-09 21:48:03 -0300

    estilo: actualizar color del encabezado en el panel de usuarios

--- frontend/src/app/admin/usuarios/page.tsx
+++ frontend/src/app/admin/usuarios/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <>
             <div className="container flex flex-col gap-12 mx-auto p-16">
-                <h2 className="text-[#1e1e1e] text-2xl font-semibold">
+                <h2 className="text-primary text-2xl font-semibold">
                     PANEL DE USUARIOS
                 </h2>
             </div>
